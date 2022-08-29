d87e91ac3b32dfe45f182f2cc56f959e5c05433f
Shokhrukh <S.Huseynov1998@hotmail.com>
2022-08-29 11:59:41 +0100

    fixed errors

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 
 
-app.use("/public", express.static(__dirname + "/public"))
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 })
 
-app.listen(8080, () => {
+app.listen(8085, () => {
   console.log("The server is up and running!");
 });
